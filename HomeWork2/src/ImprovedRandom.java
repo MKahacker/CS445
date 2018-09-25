104ea3207adfebb5e7f1a50754e9009ca551d2e2
Mahmoud Kahack <mahmoudkahack@gmail.com>
2018-09-25 16:13:05 -0500

    Starting part 4

---
+++ HomeWork2/src/ImprovedRandom.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class ImprovedRandom extends Random{
+    public ImprovedRandom(){
+        super();
+    }
+
+    public ImprovedRandom(long seed){
+        super(seed);
+    }
+}
