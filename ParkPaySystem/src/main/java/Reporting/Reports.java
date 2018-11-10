8a11a7c3d1039994455b6c6690d6e705bc6aea46
Mahmoud Kahack <mkahack@otsads.iit.edu>
2018-11-10 17:00:59 -0600

    Starting reporting use case

---
+++ ParkPaySystem/src/main/java/Reporting/Reports.java
@@ -0,0 +1,11 @@
+package Reporting;
+
+import org.json.JSONArray;
+
+public class Reports {
+
+    public static JSONArray getAdmissionReport(JSONArray parks, JSONArray orders){
+      return new JSONArray();
+    }
+
+}
---
+++ ParkPaySystem/src/test/java/test/ReportingTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import Reporting.Reports;
+import org.json.JSONArray;
+import org.junit.jupiter.api.Test;
+
+public class ReportingTest {
+
+    @Test
+    public void createAdmissionsReport(){}
+
+}
