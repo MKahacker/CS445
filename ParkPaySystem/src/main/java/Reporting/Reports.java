6a540cad39151730bf57a744d719cef2d0c64bf5
Mahmoud Kahack <mkahack@otsads.iit.edu>
2018-11-11 16:31:45 -0600

    Admission report code and tests done

--- ParkPaySystem/src/main/java/Reporting/Reports.java
+++ ParkPaySystem/src/main/java/Reporting/Reports.java
@@ -3,15 +3,64 @@ package Reporting;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Reports {
 
-    public static JSONObject getAdmissionReport(JSONArray parks, JSONArray orders){
+    public static JSONObject getAdmissionReport(JSONArray parks, JSONArray order, Date startDate, Date endDate){
         JSONObject admissionReports = new JSONObject();
+        admissionReports.put("rid", Integer.toString(567));
+        admissionReports.put("name", "Admissions report");
+        if(startDate==null){
+            admissionReports.put("start_date","");
+        }
+        if(endDate == null){
+            admissionReports.put("end_date", "");
+        }
+        admissionReports.put("total_admissions",order.length());
+        admissionReports.put("detail_by_park",getOrdersForPark(order, parks));
         return admissionReports;
+    }
+
+    private static JSONArray getOrdersForPark(JSONArray order, JSONArray parks) {
+        List<Integer> parksId = returnAllParkIds(order);
+        JSONArray parkInfo = new JSONArray();
+        for(int i = 0; i < parks.length(); i++) {
+            JSONObject parkObject = new JSONObject();
+            parkObject.put("pid", parks.getJSONObject(i).get("pid"));
+            parkObject.put("name", parks.getJSONObject(i).getJSONObject("location_info").get("name"));
+            parkObject.put("admissions", countAdmissions(order, parks.getJSONObject(i).getInt("pid")));
+            parkInfo.put(parkObject);
+        }
+        return parkInfo;
+    }
+
+    private static int countAdmissions(JSONArray order, int pid) {
+        int count = 0;
+        for(int i = 0; i < order.length(); i++){
+            if(order.getJSONObject(i).getInt("pid")==pid){
+                count+=1;
+            }
+        }
+        return count;
+    }
+
+    public static List<Integer> returnAllParkIds(JSONArray order){
+        List<Integer> parkIdSet = new ArrayList<Integer>();
+        for(int i = 0; i < order.length(); i++) {
+            if (!(parkIdSet.contains(order.getJSONObject(i).getInt("pid")))) {
+                parkIdSet.add(order.getJSONObject(i).getInt("pid"));
+            }
+        }
+
+        return parkIdSet;
     }
 
     public static String getReport() {
         return "[{\"rid\":\"567\",\"name\":\"Admissions report\"}," +
                 "{\"mrid\":\"568\",\"name\":\"Revenue report\"}]";
     }
+
 }
--- ParkPaySystem/src/test/java/test/ReportingTest.java
+++ ParkPaySystem/src/test/java/test/ReportingTest.java
@@ -3,6 +3,7 @@ package test;
 
 import Reporting.Reports;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -18,19 +19,68 @@ public class ReportingTest {
 
     @Test
     public void getAdmissionsReport(){
-        assertEquals("{}", Reports.getAdmissionReport(new JSONArray(), new JSONArray()).toString());
-    /*    assertEquals("{\"rid\":\"567\"," +
-                "\"name\":\"Admissions report\"," +
-                "\"start_date\":\"\"," +
-                "\"end_date\":\"\"," +
-                "\"total_admissions\":4," +
-                "\"detail_by_park\":[{\"pid\":\"123\"," +
-                "\"name\":\"Apple River Canyon\"," +
-                "\"admissions\":2},{\"pid\":\"124\"," +
-                "\"name\":\"Castle Rock\"," +
-                "\"admissions\":2},{\"pid\":\"131\"," +
-                "\"name\":\"Mermet Lake\"," +
-                "\"admissions\":0}]}", Reports.getAdmissionReport(new JSONArray(), new JSONArray()));*/
+        String emptyParksAndOrders = "{\"end_date\":\"\",\"detail_by_park\":[],\"total_admissions\":0," +
+                "\"name\":\"Admissions report\",\"rid\":\"567\",\"start_date\":\"\"}";
+
+        assertEquals(emptyParksAndOrders, Reports.getAdmissionReport(new JSONArray(), new JSONArray(),null,null).toString());
+
+        JSONArray parks = generatePark();
+        JSONArray orders = generateOrder();
+
+        assertEquals("{\"end_date\":\"\"," +
+                "\"detail_by_park\":[{\"name\":\"Apple River Canyon\",\"pid\":\"123\"," +
+                "\"admissions\":2},{\"name\":\"Castle Rock\",\"pid\":\"124\"," +
+                "\"admissions\":2},{\"name\":\"Mermet Lake\",\"pid\":\"131\"," +
+                "\"admissions\":0}],\"total_admissions\":4," +
+                "\"name\":\"Admissions report\",\"rid\":\"567\"," +
+                "\"start_date\":\"\"}",
+                Reports.getAdmissionReport(parks, orders,null,null).toString());
     }
+
+    private JSONArray generateOrder() {
+        JSONArray orders = new JSONArray();
+        int[] oid = new int[]{751, 761, 757, 773};
+        int[] pid = new int[]{124, 124, 123, 123};
+        String[] dateInfo = new String[]{"2018-07-03", "2018-08-02", "2018-07-04", "2018-08-05"};
+        String[] type = new String[]{"car", "rv", "car", "rv"};
+        double[] amount = new double[]{4.50, 9.25, 10, 13};
+
+        for(int k = 0; k < 4; k++) {
+            JSONObject ordersInfo = new JSONObject();
+            ordersInfo.put("oid", Integer.toString(oid[k]));
+            ordersInfo.put("pid", Integer.toString(pid[k]));
+            ordersInfo.put("date", dateInfo[k]);
+            ordersInfo.put("type", type[k]);
+            ordersInfo.put("amount", amount[k]);
+            orders.put(ordersInfo);
+        }
+
+        return orders;
+    }
+
+    private JSONArray generatePark() {
+        JSONArray parks = new JSONArray();
+        int[] pids = new int[]{123, 124, 131};
+        String[] names = new String[]{"Apple River Canyon", "Castle Rock", "Mermet Lake"};
+
+        for(int i = 0; i < 3; i++) {
+            JSONObject parksInfo = new JSONObject();
+            JSONObject locationInfo = new JSONObject();
+
+            parksInfo.put("pid", Integer.toString(pids[i]));
+            locationInfo.put("name", names[i]);
+            parksInfo.put("location_info", locationInfo);
+            parks.put(parksInfo);
+        }
+        return parks;
+    }
+
+    @Test
+    public void returnAllParksIds(){
+        JSONArray orders = generateOrder();
+        assertEquals("[124, 123]", Reports.returnAllParkIds(orders).toString());
+    }
+
+
 
 }
